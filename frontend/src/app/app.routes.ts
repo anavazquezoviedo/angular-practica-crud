311ca7daaa519d23db3198409d61d1b1ce1ceb25
Ana Vazquez Oviedo <anavazquezoviedo@gmail.com>
2026-09-18 14:06:40 +0200

    feat: setup routing and layout for home component

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,27 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    //cars
+    //  user
+    //home
+    {
+        path: 'home',
+        loadComponent: () => import('./features/home/home').then(m => m.HomeComponent)
+
+    },
+    // {
+    //     path: 'cars',
+    //     // ruta de cars
+    // },
+    // {
+    //     path: 'user',
+    //     // ruta de user
+    // },
+    // {
+    //     path: '',
+    //     redirectTo: 'home',
+    //     pathMatch: 'full'
+    // }
+
+
+];
