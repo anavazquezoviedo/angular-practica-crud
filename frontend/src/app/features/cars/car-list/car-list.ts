963d8807fe1744c062a1fc18c78cb0e8f7f49628
Ana Vazquez Oviedo <anavazquezoviedo@gmail.com>
2026-09-22 12:19:15 +0200

    feat(cars) refactor car list component to use signals, to avoid table error rendering DOM

--- frontend/src/app/features/cars/car-list/car-list.ts
+++ frontend/src/app/features/cars/car-list/car-list.ts
@@ -1,23 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, signal } from '@angular/core';
 import { CarService } from '../../../core/services/car.service';
 import { CarSummary } from '../../../core/models/car-summary.model';
-import { JsonPipe } from '@angular/common';
 
 @Component({
   selector: 'app-car-list',
-  imports: [JsonPipe],
+  imports: [],
   templateUrl: './car-list.html',
   styleUrl: './car-list.css',
 })
+
 export class CarListComponent implements OnInit {
 
-  cars: CarSummary[] = [];
+cars = signal<CarSummary[]>([]);
+
 
   constructor(private carService: CarService) { }
 
   ngOnInit(): void {
     this.carService.getCars().subscribe(response => {
-      this.cars = response.items;
+      //console.log('Respuesta:', response);
+      //console.log('Items:', response.items);
+       this.cars.set(response.items);
+     // console.log('Cars asignado:', this.cars);
     })
   }
 
